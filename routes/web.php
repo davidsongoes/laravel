6d0ef809741a6d66283ff5346759fc64bc4c5e52
Davidson <davidsongoes@gmail.com>
2016-10-23 18:47:13 -0200

    Exercicios

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,30 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+use Charlie\Customer;
+
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+
+//  Listar Customers
+
+//    $customers = Charlie\Customer::paginate(10);
+////    dd($customers->toArray());
+//    echo $customers->render();
+
+//   Criar Novo Customer
+//    $customer = new Customer();
+//    $customer->name = 'Davidson Góes';
+//    $customer->city = 'Brasília';
+//    $customer->state = 'DF';
+//    $customer->birthdate = '1987-03-24 20:00:00';
+//    $customer->special_customer = 1;
+//    $customer->save();
+//    dd($customer->toArray());
+
+//    Trazer Customer ID 2
+
+    $customer = Customer::find(2);
+    dd($customer->birthdate);
 });
