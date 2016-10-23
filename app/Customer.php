4f8598e1958db8786f7b618182628ce655264b47
Davidson <davidsongoes@gmail.com>
2016-10-23 18:46:55 -0200

    Criando relacionamento e casts

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+
+    protected $casts = [
+        'special_customer' => 'boolean',
+        'birthdate' => 'date',
+    ];
+
+    public function purchases()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
 }
